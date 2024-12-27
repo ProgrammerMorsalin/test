0b8e159dca157d0c752abaad06a62b8b83b0ef2d
morsalin <mursalim1246@gmail.com>
2024-12-28 04:15:54 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}", // Include the app directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
