16bf3bad5e8d9ec4b3d379403e2de115734f34ce
Morsalin <114427595+ProgrammerMorsalin@users.noreply.github.com>
2024-12-28 06:42:07 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}", // Include the app directory
+    "./app/**/*.{js,ts,jsx,tsx}", // Include the app directory
   ],
   theme: {
     extend: {},
